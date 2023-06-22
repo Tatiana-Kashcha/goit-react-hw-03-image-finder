f3d815c06615a986803824b97c6b23118a19df16
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-22 11:37:45 +0300

    upd Serchbar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import * as s from './Searchbar.styled';
+
+class Searchbar extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleSearch(this.state.value);
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <s.Searchbar>
+        <s.Form onSubmit={this.handleSubmit}>
+          <button type="submit">
+            <s.ButtonLabel>Search</s.ButtonLabel>
+          </button>
+
+          <s.Input
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={value}
+          />
+        </s.Form>
+      </s.Searchbar>
+    );
+  }
+}
+
+export default Searchbar;
