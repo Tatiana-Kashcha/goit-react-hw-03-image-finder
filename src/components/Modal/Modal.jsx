52ef51e0785d1fbc7f095084844c5beadccb37df
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-26 19:17:53 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,26 +7,6 @@ import * as s from './Modal.styled';
 
 //   componentDidUpdate() {}
 
-//   render() {
-//     // return (
-//     //   <s.Overlay>
-//     //     <s.Modal>{this.props.children}</s.Modal>
-//     //   </s.Overlay>
-//     // );
-
-//     return (
-//       <s.Overlay>
-//         <s.Modal>
-//           <s.CloseButton type="button" onClick={closeModal()}>
-//             <IconClose width="20" heigth="20" />
-//             <s.ButtonLabel>Close</s.ButtonLabel>
-//           </s.CloseButton>
-//           {/* <img src={currentImage} alt={tags} /> */}
-//         </s.Modal>
-//       </s.Overlay>
-//     );
-//   }
-// }
 export const Modal = ({ currentImage, tags, closeModal }) => {
   return (
     <s.Overlay>
